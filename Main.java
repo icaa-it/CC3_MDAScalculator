6e914eac2cb6a9100f8287999a7efad29a49a6e5
Rica <ricagabay@gmail.com>
2026-02-04 10:32:36 +0800

    MAIN

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("MDS Calculator");
+        System.out.println("1. Multiply");
+        System.out.println("2. Divide");
+        System.out.println("3. Addition");
+        System.out.println("4. Subtract");
+        System.out.print("Choose (1-4): ");
+
+        int choice = sc.nextInt();
+
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+
+        switch (choice) {
+            case 1:  
+                break;
+            case 2: 
+                break;
+            case 3: 
+                break;
+            case 4:
+                break;
+            default:
+                System.out.println("Invalid choice!");
+        }
+        sc.close();
+    }
+}
